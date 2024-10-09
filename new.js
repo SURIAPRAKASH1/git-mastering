3b38a5ce672d798e3dad57c577be3cb1afee4e14
SURIAPRAKASH1 <suriaprakashsuri@gmail.com>
2024-10-09 02:22:08 -0700

    Revert "add console log"
    This reverts commit dad7a31309da482866325ec4a0d597dd6dd54033.

--- new.js
+++ new.js
@@ -5,3 +5,11 @@ const initialValues = {
 
 cosole.log("New console log");
 console.log("Testing git revert comment");
+
+// some feature
+
+const http = (req, res) => {
+  if (req == 200) {
+    console.log("correct route");
+  }
+};
